303f21561d20e80f4526a193df95bd77bb33f505
tetheus4546b <vinicius.souza.matheus@escola.pr.gov.br>
2026-07-03 10:37:15 +0000

    reconcertando

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-<script src="main.js">
-
     const numeroSenha = document.querySelector('.parametro-senha__texto');
     let tamanhoSenha = 12;
     numeroSenha.textContent = tamanhoSenha;
 
     const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-    console.log(botoes)
-
+    
     botoes[0].onclick = diminuiTamanho;
+    botoes[1].onclick = aumentaTamanho;
 
     function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1
-    numeroSenha.textContent = tamanhoSenha
-}
+        tamanhoSenha = tamanhoSenha-1
+        numeroSenha.textContent = tamanhoSenha
+        
+    }
+    function aumentaTamanho(){
+        tamanhoSenha = tamanhoSenha+1
+        numeroSenha.textContent = tamanhoSenha
+    }
 
-</script>
+
+        console.log(botoes)
+
